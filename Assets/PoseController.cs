8707947f71c4433e71b56b483d6282e270fd17a7
Vincenzo-Rinaldi <vincenzorin@gmail.com>
2020-06-07 14:17:05 +0100

    Ragdoll, XR packages, scenes
    - Added ragdoll for the male char
    - Added XR packages
    - Set up of project using Oculus and XR plugins
    - Forniture, boundaries and other elements created
    - Teleport and interaction working

---
+++ Assets/PoseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseController : MonoBehaviour
+{
+    public GameObject PoseGameObject;
+    public HumanDescription humanDescription;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Avatar avatar = AvatarBuilder.BuildHumanAvatar(PoseGameObject, new HumanDescription());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
